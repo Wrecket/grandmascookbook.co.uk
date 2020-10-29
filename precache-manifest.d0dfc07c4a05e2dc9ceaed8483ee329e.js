6f25d303e6a7a47b3f48517eef5f5939856792d8
Matthew Fuller <matt.j.fuller@outlook.com>
2020-10-29 20:48:27 +0000

    Updates

---
+++ precache-manifest.d0dfc07c4a05e2dc9ceaed8483ee329e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f52cc4dbcd6373567b41217d89088107",
+    "url": "/Wrecket/grandmascookbook.co.uk/index.html"
+  },
+  {
+    "revision": "5da9a65118ed7969af6d",
+    "url": "/Wrecket/grandmascookbook.co.uk/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "25b53506fea23826936e",
+    "url": "/Wrecket/grandmascookbook.co.uk/static/css/main.7d01d949.chunk.css"
+  },
+  {
+    "revision": "5da9a65118ed7969af6d",
+    "url": "/Wrecket/grandmascookbook.co.uk/static/js/2.5e3dcdf8.chunk.js"
+  },
+  {
+    "revision": "355a40548b3071faf7a6b437298ae294",
+    "url": "/Wrecket/grandmascookbook.co.uk/static/js/2.5e3dcdf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25b53506fea23826936e",
+    "url": "/Wrecket/grandmascookbook.co.uk/static/js/main.1867d948.chunk.js"
+  },
+  {
+    "revision": "30b0315545790356c1d7",
+    "url": "/Wrecket/grandmascookbook.co.uk/static/js/runtime-main.1095f26e.js"
+  },
+  {
+    "revision": "d8eebce94d26a9a2643778e441982672",
+    "url": "/Wrecket/grandmascookbook.co.uk/static/media/tab.d8eebce9.png"
+  }
+]);
